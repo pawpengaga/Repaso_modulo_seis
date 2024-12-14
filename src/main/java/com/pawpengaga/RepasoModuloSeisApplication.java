b19b10215b26c3a07da76495d61a685568af1d0e
Erick Rivera <yomellamoerick.771@gmail.com>
2024-12-14 11:57:41 -0300

    primer commit

---
+++ src/main/java/com/pawpengaga/RepasoModuloSeisApplication.java
@@ -0,0 +1,13 @@
+package com.pawpengaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepasoModuloSeisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepasoModuloSeisApplication.class, args);
+	}
+
+}
